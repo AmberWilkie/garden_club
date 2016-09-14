7d05ce8429a74e5f8bb5e7e0abd3bb329aaf6413
Amber <amber@amberwilkie.com>
2016-09-14 11:12:26 +0200

    Set up a single private page for authorized viewers to get to

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :home, action: :index
-  get 'private', controller: :application, action: :private
+  get 'private', controller: :home, action: :private
 end
