e6d392238552ffdb8b211ce3cb210db591ef34aa
Amber <amber@amberwilkie.com>
2016-09-14 11:05:46 +0200

    Write acc tests for user authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :home, action: :index
+  get 'private', controller: :application, action: :private
 end
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -15,5 +15,23 @@ When(/^I click the "([^"]*)" button$/) do |button|
 end
 
 Then(/^I should be on the "([^"]*)" page$/) do |page|
-  expect(current_path).to eq registration_index_path
+  set_goto(page)
+  expect(current_path).to eq @goto
+end
+
+When(/^I click the "([^"]*)" link$/) do |link|
+  click_link(link)
+end
+
+Given(/^I am logged in$/) do
+  visit new_user_session_path
+  fill_in "Email", with: 'anna@random.com'
+  fill_in "Password", with: 'password'
+  click_button "Log in"
+end
+
+def set_goto(page)
+  @goto = new_user_registration_path if page == "registration"
+  @goto = edit_user_registration_path if page == "Edit Profile"
+  @goto = profile_path if page == "Profile"
 end
