50d721e2e2c95fcc2f42beea452cd4f8754e92f2
Amber <amber@amberwilkie.com>
2016-09-14 11:08:48 +0200

    Refactor basic-steps visit and am-on-the page steps

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,7 @@
 Given(/^I am on the "([^"]*)" page$/) do |page|
-  visit new_user_registration_path
+  set_goto(page)
+  visit @goto
+  expect(current_path).to eq @goto
 end
 
 Then(/^I should see "([^"]*)"$/) do |text|
@@ -31,6 +33,7 @@ Given(/^I am logged in$/) do
 end
 
 def set_goto(page)
+  @goto = root_path if page == "home"
   @goto = new_user_registration_path if page == "registration"
   @goto = edit_user_registration_path if page == "Edit Profile"
   @goto = profile_path if page == "Profile"
